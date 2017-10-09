b31963f990422538649edd7b84bf90ca837acbdf
David Martelli <teamnitros@aol.com>
2017-10-09 17:05:40 -0400

    Mr Basil 3 Pod

---
+++ AquagardenMain.py
@@ -0,0 +1,31 @@
+# Title: Aquagarden main python file.
+# Version: 0.1
+#
+# Description: This file will be the main driving file for a 3 pod hydroponic garden using a raspberry pi zero w as a
+#              driving board
+#
+# Project Components: raspberry pi zero w, parastaltic pump, 120v ->12v ->5v converter, LED light matrix, aerator pump,
+#                     main water flow pump, air fan
+#
+# Component GPIO mapping:
+
+# Aerator pump  : 11
+# nutrient pump : 12
+# Grow Light    : 13
+# Water pump    : 15
+# Flow Sensor 1 : 16
+# Flow Sensor 2 : 18
+# lux sensor    : 29
+# Plant fan     : 33
+
+#---------------begin Startup Code-----------
+
+#Check for water flow
+
+#Check for grow Light
+
+#Start Plant Fan
+
+#-----------------Main Program Loop------------
+
+
